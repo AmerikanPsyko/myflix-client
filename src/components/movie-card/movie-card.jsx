636bc2cef7b346d5bb8584dc0958ba0cafc248b8
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-05-29 01:00:02 -0700

    update
    saving just in case

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
     render() {
@@ -7,5 +8,17 @@ export class MovieCard extends React.Component {
       return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
     }
   }
+
+  MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      Genre: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      Director: PropTypes.string.isRequired,
+      ImagePath: PropTypes.string.isRequired,
+      Feature: propTypes.bool.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+  };
 
   
